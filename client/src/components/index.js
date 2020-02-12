e876f976a3ed9aa0f18c76f695dc7ef6dae0cca6
George <admin@georgesimos.com>
2020-02-12 22:52:34 +0200

    WithLayoutRoute HOC

---
+++ client/src/components/index.js
@@ -0,0 +1,5 @@
+export { default as BurgeButton } from './BurgerButton/BurgerButton';
+export { default as Footer } from './Footer/Footer';
+export { default as Layout } from './Layout/Layout';
+export { default as MobileNavBar } from './MobileNavBar/MobileNavBar';
+export { default as NavBar } from './NavBar/NavBar';
